eaa5e438e5be9c9370023f50518ea842c889451f
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-01-11 16:24:03 +0600

    feat: clear output on btn click

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hi");
+let output = document.querySelector(".display-inner");
+let clearBtn = document.querySelector(".ac > button");
+
+let clearOutput = () => {
+  output.innerText = 0;
+};
+
+clearBtn.addEventListener("click", clearOutput);
