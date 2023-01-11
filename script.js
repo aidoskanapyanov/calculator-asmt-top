bfbc8899f68a2ebb3ca8ee7b265cd2af980c385d
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-01-11 16:34:14 +0600

    feat: type numbers

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let output = document.querySelector(".display-inner");
 let clearBtn = document.querySelector(".ac > button");
+let numbers = document.querySelectorAll(".num > button");
 
-let clearOutput = () => {
-  output.innerText = 0;
+console.log(numbers);
+
+// clear display output
+let clearOutput = () => (output.innerText = 0);
+
+// type number to display
+let typeNumber = (e) => {
+  if (output.innerHTML == 0) {
+    output.innerHTML = e.target.innerHTML;
+  } else {
+    output.innerHTML += e.target.innerHTML;
+  }
 };
+
+// Adding event listeners to buttons
+numbers.forEach((el) => {
+  el.addEventListener("click", typeNumber);
+});
 
 clearBtn.addEventListener("click", clearOutput);
